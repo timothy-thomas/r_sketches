316a7bd6b5f74be946669a6c2bab6bb8a0e1f5ee
Tim Thomas <tthomas@metamorphsoftware.com>
2017-01-31 13:11:37 -0600

    Finished dynamic specification of tabs in 'AddMultipleTabs.'

--- AddMultipleTabs/app.R
+++ AddMultipleTabs/app.R
@@ -1,17 +1,24 @@
+
+
 server <- function(input, output) {
-  output$distPlot <- renderPlot({
-    hist(rnorm(input$obs), col = 'darkgray', border = 'white')
+  output$plot <- renderPlot({
+    hist(rnorm(50), col = 'darkgray', border = 'white')
   })
+  output$summary <- renderText("The summary page!")
+  output$table <- renderTable(iris)
 }
+
+tabs <- list(plo=tabPanel("Plot", div(br(), plotOutput("plot"))),
+             sum=tabPanel("Summary", div(br(), h4("My Summary"), verbatimTextOutput("summary"))),
+             tab=tabPanel("Table", div(br(), h4("My Table"), tableOutput("table"))))
+
+# print(tabs$plo)
 
 ui <- fluidPage(
   mainPanel(
     br(),
-    tabsetPanel(
-      tabPanel("Plot", plotOutput("plot")),
-      tabPanel("Summary", verbatimTextOutput("summary")),
-      tabPanel("Table", tableOutput("table"))
-    )
+    # tabsetPanel(tabs$plo, tabs$sum, tabs$tab)
+    do.call(tabsetPanel, unname(tabs))
   )
 )
 
